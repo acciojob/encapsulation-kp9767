31c0acfe8d57a5d0ecfea42b18e2adbd3f49eafc
unknown <kalpeshpatil19999@gmail.com>
2023-08-13 23:43:03 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main (String args []){
+        RWOnly obj=new RWOnly("prasad");
+        // Error: 'name' has private access in RWOnly
+        // obj.name = "New Name";
+
+        // Error: 'name' has private access in RWOnly
+        // System.out.println(obj.name);
+
+        // use a setter to set name
+        obj.setName("nikhil");
+
+        //use a getter to get name
+        System.out.println("name: "+obj.getName());
+    }
 }
